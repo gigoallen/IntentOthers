e6194817c9ffdffa35ee00690069f8e777e74a97
gigoallen <gigo@hk.edu.tw>
2020-05-25 10:50:17 +0800

    完成專案

--- app/src/main/java/com/gigoallen/intentothers/MainActivity.java
+++ app/src/main/java/com/gigoallen/intentothers/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gigoallen.intentothers;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        switch (v.getId()){
+            case R.id.btnEmail:
+                intent.setData(Uri.parse("mailto:gigo@hk.edu.tw"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "好久不見!");
+                intent.putExtra(Intent.EXTRA_TEXT, "最近好嗎?基哥");
+
+                break;
+            case R.id.btnWeb:
+                intent.setData(Uri.parse("http://www.csim.hk.edu.tw/main.php"));
+                break;
+        }
+
+
+        startActivity(intent);
+
     }
 }
